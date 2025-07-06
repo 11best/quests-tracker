bd3bb9e60d3b2cab05631bb05f997f47dfb3a013
11best <asama_b@outlook.com>
2025-07-06 16:47:44 +0700

    change domain repository adventurer, guild commander param type from model to entity

--- src/domain/repositories/adventurers.rs
+++ src/domain/repositories/adventurers.rs
@@ -1,7 +1,4 @@
-use crate::domain::{
-    entities::adventurers::AdventurerEntity,
-    value_objects::adventurer_model::RegisterAdventurerModel,
-};
+use crate::domain::entities::adventurers::{AdventurerEntity, RegisterAdventurerEntity};
 
 use anyhow::Result;
 use axum::async_trait;
@@ -10,6 +7,6 @@ use mockall::automock;
 #[async_trait]
 #[automock]
 pub trait AdventurersRepository {
-    async fn register(&self, register_adventurer_model: RegisterAdventurerModel) -> Result<i32>;
+    async fn register(&self, register_adventurer_entity: RegisterAdventurerEntity) -> Result<i32>;
     async fn find_by_username(&self, username: String) -> Result<AdventurerEntity>;
 }
