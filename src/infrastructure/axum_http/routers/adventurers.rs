239a75becb956519a271c0c479a5b3c0db58ca1b
11best <asama_b@outlook.com>
2025-07-07 14:52:12 +0700

    implement register for adventurers

--- src/infrastructure/axum_http/routers/adventurers.rs
+++ src/infrastructure/axum_http/routers/adventurers.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use axum::{Json, Router, extract::State, response::IntoResponse, routing::post};
+use axum::{Json, Router, extract::State, http::StatusCode, response::IntoResponse, routing::post};
 
 use crate::{
     application::usecases::adventurers::AdventurersUseCase,
@@ -23,11 +23,21 @@ pub fn routes(db_pool: Arc<PgPoolSquad>) -> Router {
 }
 
 pub async fn register<T>(
-    State(adventurer_use_case): State<Arc<AdventurersUseCase<T>>>,
+    State(adventurers_use_case): State<Arc<AdventurersUseCase<T>>>,
     Json(register_adventurer_model): Json<RegisterAdventurerModel>,
 ) -> impl IntoResponse
 where
     T: AdventurersRepository + Send + Sync,
 {
-    // unimplemented!()
+    match adventurers_use_case
+        .register(register_adventurer_model)
+        .await
+    {
+        Ok(adventurer_id) => (
+            StatusCode::CREATED,
+            format!("Register adventurer id: {} successfully", adventurer_id),
+        )
+            .into_response(),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
+    }
 }
